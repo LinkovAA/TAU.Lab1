43626418af767f938a5441b2d1d55c122ce216ee
LinkovAA <alexlinkofff@yandex.ru>
2021-11-26 01:30:16 +0300

    commit_3

--- Lab2.py
+++ Lab2.py
@@ -16,7 +16,7 @@ def chFeedback():
     aperiodicFlexible = 'Апериодическая гибкая'
     # Переменная для проверки правильности ввода команды
     newChoiceFeedback = True
-    # Цикл для проверки правильности ввода команды и для присвоения переменной feedbackType названия типа обратной связи
+    # Цикл для проверки правильности ввода команды и для присвоения переменной "feedbackType" названия типа обратной связи
     while newChoiceFeedback:
         print(color.Style.RESET_ALL)
         # Выбор типа обратной связи
@@ -52,7 +52,7 @@ def chTurbine():
     steamTurbine = 'Паровая турбина'
     # Переменная для проверки правильности ввода команды
     newChoiceTurbine = True
-    # Цикл для проверки правильности ввода команды и для присвоения переменной turbine названия типа турбины
+    # Цикл для проверки правильности ввода команды и для присвоения переменной "turbine" названия типа турбины
     while newChoiceTurbine:
         print(color.Style.RESET_ALL)
         # Выбор типа турбины
@@ -78,12 +78,66 @@ def chTurbine():
 
 feedback = chFeedback()
 turbine = chTurbine()
-print('Элементы исследуемой системы автоматического управления:\n'
+print('\nЭлементы исследуемой системы автоматического управления:\n'
       '1. Усилительно-исполнительный орган;\n'
       '2. ' + turbine + ';\n'
       '3. Генератор;\n'
       '4. ' + feedback + '.\n')
 
+# Функция для ввода коэффициентов передаточной функции исполнительного органа
+def chioceOrgan():
+    oChoice = True
+    # Цикл для проверки ввода коэффициентов передаточной функции
+    while oChoice:
+        print(color.Style.RESET_ALL)
+        oChoice = False
+        print('Введите коэффициенты передаточной функции исполнительного органа:')
+        k = input('Пожалуйста, введите коэффициент "k": ')
+        T = input('Пожалуйста, введите коэффициент "T": ')
+        # Конструкция try-except для проверки правильности ввода коэффициентов
+        try:
+            k = float(k)
+            T = float(T)
+            wfOrgan = matlab.tf([k], [T, 1])
+        except ValueError:
+            print(color.Fore.RED + '\nПожалуйста, введите числовое значение!\n')
+            oChoice = True
+    return wfOrgan
+
+
+# Функция для ввода коэффициентов передаточной функции турбины
+def chioceTurbine():
+    tChoice = True
+    # Цикл для проверки ввода коэффициентов передаточной функции
+    while tChoice:
+        print(color.Style.RESET_ALL)
+        tChoice = False
+        print('Введите коэффициенты передаточной функции исполнительного органа:')
+        if turbine == 'Гидротурбина':
+            T1 = input('Пожалуйста, введите постоянную времени гидротурбины: ')
+            T2 = input('Пожалуйста, введите постоянную времени генератора: ')
+            try:
+                T1 = float(T1)
+                T2 = float(T2)
+                wfTurbine = matlab.tf([0.01*T1, 1], [0.05*T2, 1])
+            except ValueError:
+                print(color.Fore.RED + '\nПожалуйста, введите числовое значение!\n')
+                tChoice = True
+        elif turbine == 'Паровая турбина':
+            k = input('Пожалуйста, введите коэффициент усиления: ')
+            T = input('Пожалуйста, введите постоянную времени: ')
+            try:
+                k = float(k)
+                T = float(T)
+                wfTurbine = matlab.tf([k], [T, 1])
+            except ValueError:
+                print(color.Fore.RED + '\nПожалуйста, введите числовое значение!\n')
+                tChoice = True
+    return wfTurbine
 
 
 
+
+
+wOrgan = chioceOrgan()
+wTurbine = chioceTurbine()
